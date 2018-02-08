f2230819fcd0626b08d00f7378d0a85715d879c1
Andei Salanoi <a.salanoi@godeltech.com>
2018-02-08 13:40:28 +0300

    SonarQube download script added

--- SonarQube/Download-Latest-Sonar-Qube-Runner.ps1
+++ SonarQube/Download-Latest-Sonar-Qube-Runner.ps1
@@ -6,18 +6,24 @@ if (Test-Path -Path $sonarQubeRunnerFolder) {
 }
 
 Write-Output "Folder $sonarQubeRunnerFolder doesn't exists. Creating folder..."
-
 New-Item -ItemType Directory -Path $sonarQubeRunnerFolder | Out-Null
 
 Write-Output "Resolving latest version of runner..."
-$webRequestResult = Invoke-WebRequest -Uri https://api.github.com/repos/SonarSource/sonar-scanner-msbuild/releases -OutFile "sonar-qube-releases.json" -PassThru
+$webRequestTempFile = "$sonarQubeRunnerFolder\sonar-qube-releases.json"
+$webRequestResult = Invoke-WebRequest -Uri https://api.github.com/repos/SonarSource/sonar-scanner-msbuild/releases -OutFile $webRequestTempFile -PassThru
 $releasedVersion = ConvertFrom-Json -InputObject $webRequestResult.Content
 
-$downloadUrl = $releasedVersion[0].zipball_url
-$downloadedFilePath = ($sonarQubeRunnerFolder + '\sonarQubeRunner.zip')
-
 Write-Output "Downloading runner from $downloadUrl to $downloadedFilePath ..."
+$downloadUrl = $releasedVersion[0].assets[0].browser_download_url
+$downloadedFilePath = ($sonarQubeRunnerFolder + '\sonarQubeRunner.zip')
 Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadedFilePath
 
 Write-Output "Unpacking runner..."
+Expand-Archive $downloadedFilePath -DestinationPath $sonarQubeRunnerFolder
+
+Write-Output "Cleaning up..."
+Remove-Item $downloadedFilePath -Force
+Remove-Item $webRequestTempFile -Force
+
+Write-Output "Done."
 
